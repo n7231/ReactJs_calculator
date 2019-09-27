6277e9c0a1f3ddfd55ad32f749e4422c6bc05cd3
Anni <islam.tania2019@outlook.com>
2019-09-27 19:04:47 +0300

    Some changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import "./App.css";
 import { Button } from "./components/Button";
 import { Input } from "./components/Input";
 import { DeleteButton } from "./components/DeleteButton";
-// import * as math from "mathjs";
-const math = require('mathjs');
-// import {eval} from "mathjs";
+
 
 class App extends Component {
   constructor(props) {
@@ -21,44 +19,53 @@ class App extends Component {
   };
 
   handleEqual = () => {
+    // to calculate inputted numbers
     this.setState({ input: eval(this.state.input) });
   };
 
   render() {
     return (
-      <div className="app">
-        <div className="calc-wrapper">
-          <Input input={this.state.input} />
-          <div className="row">
-            <Button handleClick={this.addToInput}>7</Button>
-            <Button handleClick={this.addToInput}>8</Button>
-            <Button handleClick={this.addToInput}>9</Button>
-            <Button handleClick={this.addToInput}>/</Button>
-          </div>
-          <div className="row">
-            <Button handleClick={this.addToInput}>4</Button>
-            <Button handleClick={this.addToInput}>5</Button>
-            <Button handleClick={this.addToInput}>6</Button>
-            <Button handleClick={this.addToInput}>*</Button>
-          </div>
-          <div className="row">
-            <Button handleClick={this.addToInput}>1</Button>
-            <Button handleClick={this.addToInput}>2</Button>
-            <Button handleClick={this.addToInput}>3</Button>
-            <Button handleClick={this.addToInput}>+</Button>
-          </div>
-          <div className="row">
-            <Button handleClick={this.addToInput}>.</Button>
-            <Button handleClick={this.addToInput}>0</Button>
-            <Button handleClick={() => this.handleEqual()}>=</Button>
-            <Button handleClick={this.addToInput}>-</Button>
-          </div>
-          <div className="row">
-            <DeleteButton 
-              handleClear={() => this.setState({ input: "" })}
-            >
-              Clear
+      <div className="container">
+        <div className="image"></div> 
+        <div className="row">
+          <div className="col-6"></div>
+          <div className="col-6">
+            <div className="app">
+              <div className="calc-wrapper">
+                <Input input={this.state.input} />
+                <div className="row">
+                  <Button handleClick={this.addToInput}>7</Button>
+                  <Button handleClick={this.addToInput}>8</Button>
+                  <Button handleClick={this.addToInput}>9</Button>
+                  <Button handleClick={this.addToInput}>/</Button>
+                </div>
+                <div className="row">
+                  <Button handleClick={this.addToInput}>4</Button>
+                  <Button handleClick={this.addToInput}>5</Button>
+                  <Button handleClick={this.addToInput}>6</Button>
+                  <Button handleClick={this.addToInput}>*</Button>
+                </div>
+                <div className="row">
+                  <Button handleClick={this.addToInput}>1</Button>
+                  <Button handleClick={this.addToInput}>2</Button>
+                  <Button handleClick={this.addToInput}>3</Button>
+                  <Button handleClick={this.addToInput}>+</Button>
+                </div>
+                <div className="row">
+                  <Button handleClick={this.addToInput}>.</Button>
+                  <Button handleClick={this.addToInput}>0</Button>
+                  <Button handleClick={() => this.handleEqual()}>=</Button>
+                  <Button handleClick={this.addToInput}>-</Button>
+                </div>
+                <div className="row">
+                  <DeleteButton
+                    handleClear={() => this.setState({ input: "" })}
+                  >
+                    Clear
             </DeleteButton>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
