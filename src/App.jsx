d14f8b4767e3828249a9e9d985d73a9ddc85d4d5
Anni <islam.tania2019@outlook.com>
2019-09-29 18:07:23 +0300

    more changes

--- src/App.jsx
+++ src/App.jsx
@@ -25,11 +25,16 @@ class App extends Component {
 
   render() {
     return (
-      <div className="container-fluid" id="mainContainer">
+      <div className="container-fluid mainContainer">
         <div className="row">
-          <div className="col-6"></div>
+          <div className="col-6">
+            <h1 class="animated infinite bounce delay-2s">React.js_Calculator</h1>
+            <h2>UI web design</h2>
+            <h3>Tania Islam</h3>
+          </div>
           <div className="col-6">
             <div className="app">
+            <div className Style={{marginTop: '20px', paddingTop: '20px' , backgroundcolor: '#888888'}}></div>
                <div>
                  <div className="calc-wrapper animated fadeInDown">
                    <Input input={this.state.input} />
@@ -54,7 +59,7 @@ class App extends Component {
                   <div className="calulatorRow">
                     <Button handleClick={this.addToInput}>.</Button>
                     <Button handleClick={this.addToInput}>0</Button>
-                    <Button handleClick={() => this.handleEqual()}>=</Button>
+                    <Button handleClick={() => this.handleEqual()} >=</Button>
                     <Button handleClick={this.addToInput}>-</Button>
                   </div>
                   <div className="calulatorRow">
